8e8c8578ec270b05cb6e6db7325c90847d38eec4
cheny <cheny@test.com>
2024-09-13 10:34:57 +0800

    first commit

---
+++ src/main/java/com/cheny/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.cheny.springbootinit.mapper;
+
+import com.cheny.springbootinit.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author chen
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-01-29 21:38:48
+* @Entity com.cheny.springbootinit.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
