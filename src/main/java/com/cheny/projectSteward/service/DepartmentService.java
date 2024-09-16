99d3a98a190763606cebd52de40262c4b4e039fd
cheny <cheny@test.com>
2024-09-16 23:06:51 +0800

    院系类基本功能实现

--- src/main/java/com/cheny/projectSteward/service/DepartmentService.java
+++ src/main/java/com/cheny/projectSteward/service/DepartmentService.java
@@ -1,13 +1,51 @@
 package com.cheny.projectSteward.service;
 
-import com.cheny.projectSteward.model.entity.Department;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.cheny.projectSteward.model.dto.department.DepartmentQueryRequest;
+import com.cheny.projectSteward.model.entity.Department;
+import com.cheny.projectSteward.model.vo.department.DepartmentVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
-* @author chen
-* @description 针对表【department(院系表)】的数据库操作Service
-* @createDate 2024-09-13 23:52:07
-*/
+ * 院系服务
+ *
+ */
 public interface DepartmentService extends IService<Department> {
 
+    /**
+     * 校验数据
+     *
+     * @param department
+     * @param add 对创建的数据进行校验
+     */
+    void validDepartment(Department department, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param departmentQueryRequest
+     * @return
+     */
+    QueryWrapper<Department> getQueryWrapper(DepartmentQueryRequest departmentQueryRequest);
+    
+    /**
+     * 获取院系封装
+     *
+     * @param department
+     * @param request
+     * @return
+     */
+    DepartmentVO getDepartmentVO(Department department, HttpServletRequest request);
+
+    /**
+     * 分页获取院系封装
+     *
+     * @param departmentPage
+     * @param request
+     * @return
+     */
+    Page<DepartmentVO> getDepartmentVOPage(Page<Department> departmentPage, HttpServletRequest request);
 }
